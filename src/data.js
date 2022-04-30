666563b5e3b0d8c3897313e8571e0111f5b7e907
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-04-30 22:08:43 +0200

    done with routing process and page navigation configuration

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ export const posts = [
       "Sports fans will love our huge selection of basketball games. You can play quick arcade challenges or in-depth simulation challenges. Choose your favorite court, pick a player, and get ready to shoot some hoops! Our collection puts you in control of star NBA players, streetball athletes, and newbies. You’ll be challenged to make 3-pointers, score huge slam dunks, and beat your opponents in 4 quarters. Exciting sports challenges are waiting for you!All our Online Basketball Games can be played for free on your PC. You don't have to download our games, just play them right away in your browser. Most of our games can also be played on your mobile phone or tablet. Enjoy scoring three-pointers here on Poki!Our collection is full of countless types of sporting bouts. You can play prison basketball, a 3-way shootout, and even Olympic challenges! All of our basketball games feature colorful graphics, easy-to-learn controls, and highly addictive gameplay. You’ll be completely immersed in high-octane sports adventures. Our collection will fire you up, put your skills to the test, and give you the chance to become an NBA star. Sink some 3-pointers today!",
   },
   {
-    id: 1,
+    id: 2,
     title:
       'Sports fans will love our huge selection of basketball games. You can play quick arcade challenges or in-depth simulation challenges.',
     img: 'https://cdn.pixabay.com/photo/2013/07/13/09/46/basketball-155997_960_720.png',
@@ -20,7 +20,7 @@ export const posts = [
       "Sports fans will love our huge selection of basketball games. You can play quick arcade challenges or in-depth simulation challenges. Choose your favorite court, pick a player, and get ready to shoot some hoops! Our collection puts you in control of star NBA players, streetball athletes, and newbies. You’ll be challenged to make 3-pointers, score huge slam dunks, and beat your opponents in 4 quarters. Exciting sports challenges are waiting for you!All our Online Basketball Games can be played for free on your PC. You don't have to download our games, just play them right away in your browser. Most of our games can also be played on your mobile phone or tablet. Enjoy scoring three-pointers here on Poki!Our collection is full of countless types of sporting bouts. You can play prison basketball, a 3-way shootout, and even Olympic challenges! All of our basketball games feature colorful graphics, easy-to-learn controls, and highly addictive gameplay. You’ll be completely immersed in high-octane sports adventures. Our collection will fire you up, put your skills to the test, and give you the chance to become an NBA star. Sink some 3-pointers today!",
   },
   {
-    id: 1,
+    id: 3,
     title:
       'Sports fans will love our huge selection of basketball games. You can play quick arcade challenges or in-depth simulation challenges.',
     img: 'https://cdn.pixabay.com/photo/2013/03/21/15/52/basketball-95607_960_720.jpg',
